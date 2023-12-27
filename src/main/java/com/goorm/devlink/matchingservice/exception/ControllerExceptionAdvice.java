6cc2726b2da713e95d20b8ce996aba3d6d847af9
mgKang3646 <lordofkangs@naver.com>
2023-12-28 06:06:53 +0900

    feat: exception 처리 클래스 추가

---
+++ src/main/java/com/goorm/devlink/matchingservice/exception/ControllerExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.goorm.devlink.matchingservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice(basePackages = "com.goorm.devlink.matchingservice.controller")
+public class ControllerExceptionAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResult> noSuchElementExceptionHandler(NoSuchElementException exception,
+                                                                     HttpServletRequest request){
+        return ResponseEntity.badRequest()
+                .body(ErrorResult.getInstance(exception.getMessage(), request.getRequestURL().toString()));
+    }
+}
