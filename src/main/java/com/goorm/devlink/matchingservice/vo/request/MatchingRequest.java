6e4d9ad0e80fb8df30e8edc51cd5d809c2203f46
mgKang3646 <lordofkangs@naver.com>
2024-01-01 02:44:18 +0900

    chore: @JsonProperty 추가

--- src/main/java/com/goorm/devlink/matchingservice/vo/request/MatchingRequest.java
+++ src/main/java/com/goorm/devlink/matchingservice/vo/request/MatchingRequest.java
@@ -1,5 +1,6 @@
 package com.goorm.devlink.matchingservice.vo.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.goorm.devlink.matchingservice.vo.MatchingType;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,9 +17,12 @@ public class MatchingRequest {
     private LocalDateTime startTime;
     @NotNull(message = "러닝타임을 입력해주세요.")
     private Integer unitTimeCount;
+
     @NotNull(message = "멘토링 대상의 유형을 선택해주세요 ( MENTOR/MENTEE )")
+    @JsonProperty("type")
     private MatchingType matchingType;
 
+    @JsonProperty("place")
     private String mentoringPlace;
 
 
